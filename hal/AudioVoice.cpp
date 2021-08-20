3442a932ef99979a34af7f13b3a222b96554d0f4
Tejaswinee Langhe <quic_tlanghe@quicinc.com>
2021-08-20 23:11:44 +0530

    audio-ar: compilation fix for KS sync path change.
    In current makefiles, opensource path and common path are hard coded.
    Instead, include header libraries and shared libraries
    where ever possible in order to make the path relocatable.
    
    Change-Id: Ib4805f14b6b11b6a2bd32021eab5b55d1f21b8a6

--- hal/AudioVoice.cpp
+++ hal/AudioVoice.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2019-2021, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -26,6 +25,11 @@
  * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Changes from Qualcomm Innovation Center are provided under the following license:
+ *
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 #define LOG_TAG "AHAL: AudioVoice"
@@ -38,7 +42,7 @@
 #include "AudioVoice.h"
 #include "PalApi.h"
 #include "AudioCommon.h"
-#include <audio_extn/AudioExtn.h>
+#include <AudioExtn.h>
 
 #ifndef AUDIO_MODE_CALL_SCREEN
 #define AUDIO_MODE_CALL_SCREEN 4
