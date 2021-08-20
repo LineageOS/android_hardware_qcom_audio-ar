933eafe7592398b1bf75fcafdc17d8332a071f7c
Tejaswinee Langhe <quic_tlanghe@quicinc.com>
2021-08-20 23:11:44 +0530

    audio-ar: compilation fix for KS sync path change.
    In current makefiles, opensource path and common path are hard coded.
    Instead, include header libraries and shared libraries
    where ever possible in order to make the path relocatable.
    
    Change-Id: Ib4805f14b6b11b6a2bd32021eab5b55d1f21b8a6

--- hal/audio_extn/Android.mk
+++ hal/audio_extn/Android.mk
@@ -1,5 +1,14 @@
 LOCAL_PATH := $(call my-dir)
 
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libaudio_extn_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
+
 #-------------------------------------------
 #            Build HFP LIB
 #-------------------------------------------
@@ -30,15 +39,14 @@ LOCAL_SHARED_LIBRARIES := \
     libar-pal
 
 LOCAL_C_INCLUDES := \
-    vendor/qcom/opensource/pal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn \
+    $(LOCAL_PATH)/inc \
     external/expat/lib \
     system/media/audio_utils/include \
     $(call include-path-for, audio-route) \
 
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
+LOCAL_HEADER_LIBRARIES += libaudio_hal_headers
 include $(BUILD_SHARED_LIBRARY)
 
 #-------------------------------------------
@@ -67,15 +75,14 @@ LOCAL_SHARED_LIBRARIES := \
     libar-pal
 
 LOCAL_C_INCLUDES := \
-    vendor/qcom/opensource/pal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal \
-    vendor/qcom/opensource/audio-hal/primary-hal/hal/audio_extn \
+    $(LOCAL_PATH)/inc \
     external/expat/lib \
     system/media/audio_utils/include \
     $(call include-path-for, audio-route) \
 
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_HEADER_LIBRARIES += libsystem_headers
+LOCAL_HEADER_LIBRARIES += libaudio_hal_headers
 include $(BUILD_SHARED_LIBRARY)
 
 #-------------------------------------------
@@ -99,6 +106,8 @@ ifneq ($(filter $(TARGET_BOARD_PLATFORM), anorak anorak61), $(TARGET_BOARD_PLATF
     LOCAL_CFLAGS += -DHEALTH_AIDL
     $(warning "Using AIDL HEALTH")
 endif
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
 
 LOCAL_SHARED_LIBRARIES := \
     android.hardware.health@1.0 \
