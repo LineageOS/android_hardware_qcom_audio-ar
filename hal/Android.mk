30d131823c9e78f34ac28635d8a1987cc1ef8916
Kiran Tukaram Kadam <quic_kkadam@quicinc.com>
2024-04-25 17:30:10 +0530

    audio-hal : update HAL configs directory to support anorak61
    What : Add configs directory basis supporting new target name anorak61
    
    Why  : as variable TARGET_BOARD_PLATFORM supports anorak61 target
    
    How  : add new directory names anorak61 and added supporting files from
           Halliday 2.0 source code
    
    Change-Id: If862b1ea7aff1baf064d84a813c34dba2c16a84c

--- hal/Android.mk
+++ hal/Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-LOCAL_AUDIO_SERVICE_64 := taro kalama anorak crow
+LOCAL_AUDIO_SERVICE_64 := taro kalama anorak anorak61 crow
 
 include $(CLEAR_VARS)
 ifeq ($(call is-board-platform-in-list,$(LOCAL_AUDIO_SERVICE_64)), true)
@@ -24,7 +24,7 @@ LOCAL_VENDOR_MODULE := true
 LOCAL_ARM_MODE := arm
 
 LOCAL_VINTF_FRAGMENTS := ../configs/common/manifest_non_qmaa.xml
-ifeq ($(TARGET_BOARD_PLATFORM), anorak)
+ifeq ($(TARGET_BOARD_PLATFORM), anorak anorak61)
 $(warning "Update manifest fragement for anorak")
 LOCAL_VINTF_FRAGMENTS += ../configs/anorak/android.hardware.audio@7.1.xml
 endif
