b09be8babfd3b3f0ea762d7743df7044d22891ad
Bruno Martins <bgcngm@gmail.com>
2024-09-09 23:19:59 +0100

    hal: Guard primary audio HAL in case target provides prebuilt
    Change-Id: I49e8f21386353b7e2cc56cf7ed74213f44cd4478

--- hal/Android.mk
+++ hal/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(TARGET_PROVIDES_AUDIO_HAL),true)
 LOCAL_PATH := $(call my-dir)
 
 LOCAL_AUDIO_SERVICE_64 := taro kalama anorak pineapple pitti volcano
@@ -139,3 +140,4 @@ endif
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+endif
