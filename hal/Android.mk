bd821fef979cb85f9abf0eb8135befbb40600556
Bruno Martins <bgcngm@gmail.com>
2024-10-01 10:24:40 +0100

    hal: Move audio primary HAL guard
    The recently introduced libaudio_hal_headers module must be available,
    even if target uses prebuilt audio HAL.
    
    Change-Id: Ic8312d92b21632ccd1cb315d1fc667bbd84ad35c

--- hal/Android.mk
+++ hal/Android.mk
@@ -1,4 +1,3 @@
-ifneq ($(TARGET_PROVIDES_AUDIO_HAL),true)
 LOCAL_PATH := $(call my-dir)
 
 LOCAL_AUDIO_SERVICE_64 := taro kalama anorak pineapple pitti volcano
@@ -12,6 +11,7 @@ LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_HEADER_LIBRARY)
 
+ifneq ($(TARGET_PROVIDES_AUDIO_HAL),true)
 include $(CLEAR_VARS)
 
 ifeq ($(call is-board-platform-in-list,$(LOCAL_AUDIO_SERVICE_64)), true)
