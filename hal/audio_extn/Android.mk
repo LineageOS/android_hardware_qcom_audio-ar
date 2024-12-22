0909a1e3bade82c6e36e6252350be22c9c05b265
lordsheapshyt <lordsheapshyt@proton.me>
2024-12-22 10:48:46 +0000

    hfp: add missing libarpal_headers
    Hfp.cpp depends on PalApi.h
    If libarpal_headers is not in dependencies, build fails.
    bengal_515 ships libhfp_pal
    
    Change-Id: I89fb375764dcad65da19183008c169c9627d510e

---
+++ hal/audio_extn/Android.mk
@@ -0,0 +1,144 @@
+<<<<<<< HEAD   (0b70a90c0bd7c32aac2d5d34cbf413876eebb9bf Merge tag 'LA.VENDOR.1.0.r1-28700-WAIPIO.QSSI16.0' into stag)
+=======
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libaudio_extn_headers
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
+
+LOCAL_VENDOR_MODULE := true
+
+include $(BUILD_HEADER_LIBRARY)
+
+#-------------------------------------------
+#            Build HFP LIB
+#-------------------------------------------
+include $(CLEAR_VARS)
+
+ifeq ($(SOONG_CONFIG_android_hardware_audio_run_64bit), true)
+LOCAL_MULTILIB := 64
+endif
+
+LOCAL_MODULE := libhfp_pal
+LOCAL_VENDOR_MODULE := true
+
+ifeq ($(TARGET_BOARD_AUTO),true)
+  LOCAL_CFLAGS += -DPLATFORM_AUTO
+endif
+
+LOCAL_SRC_FILES:= Hfp.cpp
+
+LOCAL_CFLAGS += \
+    -Wall \
+    -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable
+
+LOCAL_SHARED_LIBRARIES := \
+    libaudioroute \
+    libaudioutils \
+    libcutils \
+    libdl \
+    libexpat \
+    liblog \
+    libar-pal
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/inc \
+    external/expat/lib \
+    system/media/audio_utils/include \
+    $(call include-path-for, audio-route) \
+
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES += libsystem_headers
+LOCAL_HEADER_LIBRARIES += libaudio_hal_headers
+LOCAL_HEADER_LIBRARIES += libarpal_headers
+include $(BUILD_SHARED_LIBRARY)
+
+#-------------------------------------------
+#            Build FM LIB
+#-------------------------------------------
+include $(CLEAR_VARS)
+
+ifeq ($(SOONG_CONFIG_android_hardware_audio_run_64bit), true)
+LOCAL_MULTILIB := 64
+endif
+
+LOCAL_MODULE := libfmpal
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES:= FM.cpp
+
+LOCAL_CFLAGS += \
+    -Wall \
+    -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable
+
+LOCAL_SHARED_LIBRARIES := \
+    libaudioroute \
+    libaudioutils \
+    libcutils \
+    libdl \
+    libexpat \
+    liblog \
+    libar-pal
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/inc \
+    external/expat/lib \
+    system/media/audio_utils/include \
+    $(call include-path-for, audio-route) \
+
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_HEADER_LIBRARIES += libsystem_headers
+LOCAL_HEADER_LIBRARIES += libaudio_hal_headers
+LOCAL_HEADER_LIBRARIES += libarpal_headers
+include $(BUILD_SHARED_LIBRARY)
+
+#-------------------------------------------
+#            Build BATTERY_LISTENER
+#-------------------------------------------
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := libbatterylistener
+LOCAL_VENDOR_MODULE := true
+
+LOCAL_SRC_FILES:= battery_listener.cpp
+
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable
+
+ifneq ($(filter bengal blair,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_CFLAGS += -DQTI_HEALTH
+endif
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/inc
+
+LOCAL_SHARED_LIBRARIES := \
+    android.hardware.health@1.0 \
+    android.hardware.health@2.0 \
+    android.hardware.health@2.1 \
+    android.hardware.power@1.2 \
+    libaudioutils \
+    libbase \
+    libcutils \
+    libdl \
+    libhidlbase \
+    liblog \
+    libutils \
+
+ifneq ($(filter bengal blair,$(TARGET_BOARD_PLATFORM)),)
+    LOCAL_SHARED_LIBRARIES += \
+        android.hardware.health-V1-ndk \
+        libbinder_ndk
+endif
+
+LOCAL_STATIC_LIBRARIES := libhealthhalutils
+
+include $(BUILD_SHARED_LIBRARY)
+>>>>>>> CHANGE (60b3537a3e2f7dd97c7554108227d29dfc122586 hfp: add missing libarpal_headers)
