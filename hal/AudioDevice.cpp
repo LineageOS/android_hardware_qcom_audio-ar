819819a794155833dff3627db00f04564478a29a
pjgowtham <pjgowtham@gmail.com>
2024-12-29 22:55:45 +0530

    hal: Add LVACFS microphone feature
    This feature is only used by newer oplus and nothing
    devices and is necessary for the microphone output to not be
    awfully low in all situations except calls.
    
    Change-Id: I3ae4e8b3a99765fcb052a07d1dd3082b035d62ad

--- hal/AudioDevice.cpp
+++ hal/AudioDevice.cpp
@@ -343,6 +343,9 @@ AudioDevice::~AudioDevice() {
 #ifdef AUDIO_ULTRASOUND_PROXIMITY_ENABLED
     audio_hal_con_thread_exit();
 #endif
+    if (Lvacfs::isLvacfsEnabled()) {
+        Lvacfs::getInstance().deinit();
+    }
     audio_extn_gef_deinit(adev_);
     audio_extn_sound_trigger_deinit(adev_);
     AudioExtn::battery_properties_listener_deinit();
@@ -1182,6 +1185,9 @@ int AudioDevice::Init(hw_device_t **device, const hw_module_t *module) {
     FillAndroidDeviceMap();
     FillPalDeviceMap();
     audio_extn_gef_init(adev_);
+    if (Lvacfs::isLvacfsEnabled()) {
+        Lvacfs::getInstance().init();
+    }
     adev_init_ref_count += 1;
 
     memset(&microphones, 0, sizeof(microphone_characteristics_t));
