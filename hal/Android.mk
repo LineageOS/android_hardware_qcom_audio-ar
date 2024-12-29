c01b1059aae991ef8f50b82faec25bf4d69cf221
pjgowtham <pjgowtham@gmail.com>
2024-12-29 22:55:45 +0530

    hal: Add LVACFS microphone feature
    This feature is only used by newer oplus and nothing devices and is
    necessary for the microphone output to not be awfully low in all
    situations except calls.
    
    Change-Id: I3ae4e8b3a99765fcb052a07d1dd3082b035d62ad

--- hal/Android.mk
+++ hal/Android.mk
@@ -78,6 +78,7 @@ LOCAL_SRC_FILES := \
     AudioStream.cpp \
     AudioDevice.cpp \
     AudioVoice.cpp \
+    Lvacfs.cpp \
     audio_extn/soundtrigger.cpp \
     audio_extn/Gain.cpp \
     audio_extn/AudioExtn.cpp
